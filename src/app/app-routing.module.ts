ad240b305fa8639b397772b5d225e128ecd2c403
jiangqg <1064286776@qq.com>
2019-04-29 10:45:34 +0800

    详情

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-  
-const routes: Routes = [
-{ path: 'login', component: LoginComponent },
 
-{
-  path: 'list', component: FruitsComponent},
-// { path: '', redirectTo: '/login', pathMatch: 'prefix' }
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  { path: 'list', component: FruitsComponent },
+  { path: 'detail/:id', component: FruitDetailComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'prefix' }
 ]
-      
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes,{useHash:true})
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   exports: [RouterModule]
 })
--- src/app/fruits/fruits.component.ts
+++ src/app/fruits/fruits.component.ts
@@ -1,31 +1,35 @@
 import { FruitService } from './../fruit.service';
 import { Fruit } from '../fruit';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-fruits',
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.css']
-}) 
+})
 
 export class FruitsComponent implements OnInit {
- fruits : Fruit[];
- selectedFruit:Fruit;
- 
-  constructor( private fruitService: FruitService) {
-       
-   }
+  fruits: Fruit[];
+  selectedFruit: Fruit;
+
+  constructor(
+    private fruitService: FruitService,
+    private router: Router
+  ) {
+
+  }
 
   ngOnInit() {
     this.getFruits();
   }
-  onSelect(fruit:Fruit):void{
-    this.selectedFruit= fruit;
-    console.log(this.selectedFruit, '==onSelect=')
-    }
-    getFruits(){
-     this.fruitService.getFruits().subscribe(fruits => this.fruits=fruits);
-   }
-  
+  // onSelect(fruit: Fruit): void {
+  //   this.selectedFruit = fruit;
+  //   this.router.navigateByUrl("/list");
+  // }
+  getFruits() {
+    this.fruitService.getFruits().subscribe(fruits => this.fruits = fruits);
+  }
+
 }
